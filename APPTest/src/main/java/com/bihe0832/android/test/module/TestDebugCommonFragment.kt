4f20e15afc057b67ebfcc61ce4b76467d16e9c9e
子勰 <code@bihe0832.com>
2021-02-03 16:37:25 +0800

    AAF 更新

--- APPTest/src/main/java/com/bihe0832/android/test/module/TestDebugCommonFragment.kt
+++ APPTest/src/main/java/com/bihe0832/android/test/module/TestDebugCommonFragment.kt
@@ -1,19 +1,9 @@
 package com.bihe0832.android.test.module
 
-<<<<<<< HEAD
-import android.content.Intent
-import android.view.View
-import com.bihe0832.android.base.card.tips.TipsData
-import com.bihe0832.android.common.test.item.TestItemData
-import com.bihe0832.android.framework.ZixieContext
-import com.bihe0832.android.lib.adapter.CardBaseModule
-import com.bihe0832.android.lib.lifecycle.*
-=======
 import android.view.View
 import com.bihe0832.android.base.card.tips.TipsData
 import com.bihe0832.android.common.test.item.TestItemData
 import com.bihe0832.android.lib.adapter.CardBaseModule
->>>>>>> AAF更新
 
 open class TestDebugCommonFragment : com.bihe0832.android.common.test.module.TestDebugCommonFragment() {
 
@@ -34,21 +24,4 @@ open class TestDebugCommonFragment : com.bihe0832.android.common.test.module.Tes
             })
         }
     }
-
-<<<<<<< HEAD
-    private fun startActivity(activityName: String) {
-        try {
-            val threadClazz = Class.forName(activityName)
-            val intent = Intent(context, threadClazz)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            startActivity(intent)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            ZixieContext.showToast("请确认当前运行的测试模块是否包含该应用")
-        }
-    }
-=======
-
->>>>>>> AAF更新
 }
