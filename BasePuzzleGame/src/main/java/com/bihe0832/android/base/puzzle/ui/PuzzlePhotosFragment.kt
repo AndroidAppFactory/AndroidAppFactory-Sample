d1ea00bd40adb34a4603a5df1affdff588d579a6
子勰 <code@bihe0832.com>
2021-08-02 16:58:20 +0800

    增加自定义网络图片

--- BasePuzzleGame/src/main/java/com/bihe0832/android/base/puzzle/ui/PuzzlePhotosFragment.kt
+++ BasePuzzleGame/src/main/java/com/bihe0832/android/base/puzzle/ui/PuzzlePhotosFragment.kt
@@ -9,8 +9,17 @@ import com.bihe0832.android.base.puzzle.PuzzleGameManager
 import com.bihe0832.android.common.photos.cropPhoto
 import com.bihe0832.android.common.photos.getDefaultPhoto
 import com.bihe0832.android.common.photos.getPhotosFolder
+import com.bihe0832.android.framework.ZixieContext
 import com.bihe0832.android.framework.constant.ZixieActivityRequestCode
+import com.bihe0832.android.lib.debug.DebugTools
+import com.bihe0832.android.lib.debug.InputDialogCompletedCallback
+import com.bihe0832.android.lib.download.DownloadItem
+import com.bihe0832.android.lib.download.wrapper.DownloadFile
+import com.bihe0832.android.lib.download.wrapper.SimpleDownloadListener
 import com.bihe0832.android.lib.log.ZLog
+import com.bihe0832.android.lib.request.URLUtils
+import com.bihe0832.android.lib.thread.ThreadManager
+import com.bihe0832.android.lib.ui.dialog.OnDialogListener
 import com.bihe0832.android.lib.ui.image.BitmapUtil
 
 /**
@@ -38,8 +47,68 @@ class PuzzlePhotosFragment : PhotosSelectFragment() {
         }
     }
 
+    override fun cloudPhoto() {
+        DebugTools.showInputDialog(
+                activity!!,
+                "使用网络图片进行游戏",
+                "在下方输入想要使用图片的地址并点击确定开始游戏",
+                "https://api88.net/api/img/rand/",
+                object : InputDialogCompletedCallback {
+                    override fun onInputCompleted(result: String?) {
+                        if (URLUtils.isHTTPUrl(result)) {
+                            useInternetURL(result!!)
+                        } else {
+                            ZixieContext.showToastJustAPPFront("请输入正确的图片地址")
+                        }
+                    }
+
+                }
+        )
+    }
+
+    private fun useInternetURL(url: String) {
+        DownloadFile.startDownloadWithProcess(
+                activity!!,
+                "图片加载",
+                "正在加载网络图片，加载结束将直接进入游戏",
+                url,
+                "", "",
+                canCancel = true,
+                useMobile = true,
+                listener = object : OnDialogListener {
+                    override fun onPositiveClick() {
+                        ZixieContext.showToastJustAPPFront("图片加载已切换到后台")
+                    }
+
+                    override fun onNegativeClick() {
+                        onCancel()
+                    }
+
+                    override fun onCancel() {
+                        ZixieContext.showToastJustAPPFront("图片加载已经取消")
+                    }
+                },
+                downloadListener = object : SimpleDownloadListener() {
+                    override fun onComplete(filePath: String, item: DownloadItem) {
+                        ZLog.d("onComplete:$item")
+                        ThreadManager.getInstance().runOnUIThread {
+                            activity?.cropPhoto(filePath, activity!!.getPhotosFolder() + "crop_puzzle.jpg")
+                        }
+                    }
+
+                    override fun onFail(errorCode: Int, msg: String, item: DownloadItem) {
+                        ZixieContext.showToastJustAPPFront("图片加载失败，请重试")
+                    }
+
+                    override fun onProgress(item: DownloadItem) {
+                        ZLog.d("onProgress: $item")
+                    }
+                })
+
+    }
+
     override fun customPhoto() {
-        RouterHelper.openPageByRouter(RouterConstants.MODULE_NAME_PUZZLE_GAME)
+        useInternetURL("https://api88.net/api/img/rand/")
     }
 
     override fun getHorizontalFix(): Int {
