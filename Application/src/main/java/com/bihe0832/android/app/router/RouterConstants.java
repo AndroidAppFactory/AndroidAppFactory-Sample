ffa1b7cefb5df3b604d59cdbc08290b8de02bf40
子勰 <code@bihe0832.com>
2021-08-03 23:18:24 +0800

    更新x5内核，升级基础库

--- Application/src/main/java/com/bihe0832/android/app/router/RouterConstants.java
+++ Application/src/main/java/com/bihe0832/android/app/router/RouterConstants.java
@@ -1,10 +1,7 @@
 package com.bihe0832.android.app.router;
 
 
-import com.bihe0832.android.app.about.AboutActivityKt;
-import com.bihe0832.android.common.feedback.FeedbackActivity;
 import com.bihe0832.android.common.webview.base.BaseWebviewFragment;
-import com.bihe0832.android.common.webview.WebPageActivity;
 
 /**
  * Created by hardyshi on 2017/10/25.
@@ -12,10 +9,8 @@ import com.bihe0832.android.common.webview.WebPageActivity;
 
 public class RouterConstants extends com.bihe0832.android.framework.router.RouterConstants {
 
-    public static final String MODULE_NAME_WEB_PAGE = WebPageActivity.MODULE_NAME_WEB_PAGE;
-    public static final String MODULE_NAME_FEEDBACK = FeedbackActivity.MODULE_NAME_FEEDBACK;
     public static final String INTENT_EXTRA_KEY_WEB_URL = BaseWebviewFragment.INTENT_KEY_URL;
-    public static final String MODULE_NAME_BASE_ABOUT = AboutActivityKt.MODULE_NAME_BASE_ABOUT;
+    public static final String MODULE_NAME_BASE_ABOUT = "about";
     public static final String MODULE_NAME_APK_LIST = "apklist";
     public static final String MODULE_NAME_PUZZLE_GAME = "puzzlegame";
     public static final String MODULE_NAME_PUZZLE = "puzzle";
