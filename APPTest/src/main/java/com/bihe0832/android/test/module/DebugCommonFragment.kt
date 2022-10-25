21907ae5ac1f715f31fb686cff6c95fb09e4a292
子勰 <code@bihe0832.com>
2022-10-25 22:34:58 +0800

    基础库更新升级

--- APPTest/src/main/java/com/bihe0832/android/test/module/DebugCommonFragment.kt
+++ APPTest/src/main/java/com/bihe0832/android/test/module/DebugCommonFragment.kt
@@ -5,6 +5,7 @@ import com.bihe0832.android.common.debug.item.DebugItemData
 import com.bihe0832.android.common.debug.item.DebugTipsData
 import com.bihe0832.android.common.debug.log.DebugLogActivity
 import com.bihe0832.android.common.debug.module.DebugCommonFragment
+import com.bihe0832.android.framework.ZixieContext
 import com.bihe0832.android.lib.adapter.CardBaseModule
 
 open class DebugCommonFragment : DebugCommonFragment() {
@@ -15,20 +16,33 @@ open class DebugCommonFragment : DebugCommonFragment() {
             add(DebugItemData("查看应用版本及环境", View.OnClickListener { showAPPInfo() }))
             add(DebugItemData("查看使用情况", View.OnClickListener { showUsedInfo() }))
             add(DebugItemData("查看设备信息", View.OnClickListener { showMobileInfo() }))
-            add(DebugItemData("日志管理", View.OnClickListener { startActivityWithException(DebugLogActivity::class.java) }))
+            add(
+                DebugItemData(
+                    "日志管理",
+                    View.OnClickListener { startActivityWithException(DebugLogActivity::class.java) })
+            )
             add(DebugTipsData("APPFactory支持的应用"))
             add(DebugItemData("应用信息获取") {
-                startActivityWithException("com.bihe0832.android.app.apk.MainActivity")
+                openNew("com.bihe0832.android.app.apk.MainActivity")
             })
 
             add(DebugItemData("来拼图") {
-                startActivityWithException("com.bihe0832.android.app.puzzle.MainActivity")
+                openNew("com.bihe0832.android.app.puzzle.MainActivity")
             })
 
             add(DebugItemData("M3U8下载器") {
-                startActivityWithException("com.bihe0832.android.app.m3u8.MainActivity")
+                openNew("com.bihe0832.android.app.m3u8.MainActivity")
             })
 
+        }
+    }
+
+    fun openNew(activityName: String) {
+        try {
+            startActivityWithException(activityName)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ZixieContext.showDebug("APP模块未集成")
         }
     }
 }
