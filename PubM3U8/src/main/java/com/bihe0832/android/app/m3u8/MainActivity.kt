6185dda80540f4a00fc79554091b0391f4f53bd4
子勰 <code@bihe0832.com>
2023-04-14 11:34:57 +0800

    增加下载的并发

--- PubM3U8/src/main/java/com/bihe0832/android/app/m3u8/MainActivity.kt
+++ PubM3U8/src/main/java/com/bihe0832/android/app/m3u8/MainActivity.kt
@@ -5,6 +5,8 @@ import com.bihe0832.android.app.router.RouterConstants
 import com.bihe0832.android.app.ui.AAFCommonMainActivity
 import com.bihe0832.android.base.m3u8.db.M3U8DBManager
 import com.bihe0832.android.base.m3u8.history.M3U8ListFragment
+import com.bihe0832.android.framework.ZixieContext
+import com.bihe0832.android.lib.download.wrapper.DownloadUtils
 import com.bihe0832.android.lib.router.annotation.APPMain
 import com.bihe0832.android.lib.router.annotation.Module
 
@@ -14,6 +16,7 @@ class MainActivity : AAFCommonMainActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DownloadUtils.init(this, 30, ZixieContext.isDebug())
         M3U8DBManager.init(this)
     }
 
